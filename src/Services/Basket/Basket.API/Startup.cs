9628d6923e11ed0ab13f1600f3bba23397053667
amitkanaujia <amitkanaujia@outlook.com>
2022-01-01 23:39:40 +0530

    RabbitMq added

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -34,6 +34,7 @@ namespace Basket.API
 
             //General Configuration
             services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddAutoMapper(typeof(Startup));
 
             //Grpc Configuration
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
